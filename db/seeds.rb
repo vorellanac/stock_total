2990d286fa8474b4fa6f7462d2dbaf8dc5522bdc
vorellanac <orellana.castro@gmail.com>
2016-12-20 19:54:48 -0300

    Model Category and products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Categories for Category
+
+Category.create(name: "Ferreteria", name: "Casa & Jardin", name: "Construccion", name: "Electricidad")
